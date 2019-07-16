b03d7d7778866de0087485862a85e644353ebbb0
christian-million <christianmillion93@gmail.com>
2019-07-15 21:16:17 -0700

    calc splits

--- R/calc.R
+++ R/calc.R
@@ -2,24 +2,47 @@ library(lubridate)
 
 #https://davidrroberts.wordpress.com/2016/04/04/running-pace-and-time-calculator-function-for-r/
 
+seconds_to_time <- function(sec){
+  #Need to vectorize
+  hours <- floor(sec/60/60)
+  mins <- floor(sec/60 - (hours*60))
+  sec_pretty <- round((sec) - (mins*60) - (hours*60*60))
+  out <- rep(NA, length(sec_pretty))
+  
+  for(i in seq_along(sec_pretty)){
+    if(hours[i]<1){
+      out[i] <- paste(sprintf("%02d", mins[i]), sprintf("%02d", sec_pretty[i]), sep = ':')
+    } else {
+      out[i] <- paste(sprintf("%02d", hours[i]), sprintf("%02d", mins[i]), sprintf("%02d", sec_pretty[i]), sep = ':')
+    }
+  }
+   return(out)
+}
+
 calc_pace <- function(distance, time){
   
   secs <- lubridate::period_to_seconds(lubridate::hms(time))
   pace <- secs / distance
-  hours <- floor(pace/60/60)
-  mins <- floor(pace/60 - (hours*60))
-  sec_pretty <- round((pace) - (mins*60) - (hours*60*60))
-  paste(sprintf("%02d", mins), sprintf("%02d", sec_pretty), sep = ':')
+  seconds_to_time(pace)
   
 }
 
-calc_pace(26.2, "03:30:00")
+# calc_pace(26.2, "03:30:00")
 
 
-
-calc_splits <- function(){
+calc_splits <- function(distance, time){
   
+  splits <- data.frame(distance = c(1:floor(distance), distance))
   
+  secs <- lubridate::period_to_seconds(lubridate::hms(time))
+  
+  pace <- secs / distance
+  
+  splits$sec <- splits$distance * pace
+  
+  splits$splits <- seconds_to_time(splits$sec)
+  
+  splits[c('distance', 'splits')]
 }
 
 calc_splits(26.2, "03:30:00")
